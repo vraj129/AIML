8546fce4c4ee3eb39ed58b20e9389394e52cdc43
vraj129 <vrajpatelv@protonmail.com>
2024-08-18 15:08:20 +0530

    -- Numbers excersise

---
+++ week2_3/number_ex.py
@@ -0,0 +1,17 @@
+len = 92
+width = 48.8
+area = len * width
+print(area)
+print("**********")
+packetCost = 1.49
+qty = 9
+cost = packetCost * qty
+change = 20 - cost 
+print(change)
+print("**********")
+sl = 5.5
+bathroomArea = sl**2
+cost = bathroomArea * 500
+print(cost)
+print("**********")
+print(format(17,'b'))

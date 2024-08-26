44614e7c2207d028828464335cb14cfea6b8fc79
vraj129 <vrajpatelv@protonmail.com>
2024-08-26 09:54:37 +0530

    -- string excersize [wip]

---
+++ week2_3/string_ex.py
@@ -0,0 +1,11 @@
+street ="abc street"
+city = "abc city"
+country ="abc country"
+print(street+"\n"+city+"\n"+country)
+
+print("***********")
+temp = "Earth revolves around the sun"
+print(temp[6:14])
+print(temp[-3:])
+
+print("***********")
